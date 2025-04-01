2b9faa0519426823824077862defa3cfb7927002
Ethan Blanche <blancheq@bc.edu>
2025-04-01 18:54:54 -0400

    Initial Commit

---
+++ NotesToStudy/NotesToStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesToStudyApp.swift
+//  NotesToStudy
+//
+//  Created by Ethan Blanche on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesToStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
